72b3c72860e64dd32461aeb8428896b1a6c1d6ba
gorhovakimyan38 <gor.hovakimyan2@tumo.org>
2022-03-09 23:42:54 +0400

    Final Changes

--- Final Game/Server.js
+++ Final Game/Server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
     res.redirect('index.html')
 });
 
-server.listen(3002);
+server.listen(3003);
 
 
 grassArr = []
@@ -127,13 +127,6 @@ function game() {
 
 setInterval(game, 500)
 
-var weath;
-
-function weather() {
-    weath = 'winter'
-    io.sockets.emit('weather', weath)
-}
-setInterval(weather,1000)
 
 function gameStat() {
     count = {
@@ -154,3 +147,145 @@ setInterval(gameStat, 300)
 io.on('connection', function () {
     createObject();
 });
+
+
+// functions 
+
+function winter () {
+    background='white'
+}
+io.sockets.emit('send matrix', matrix)
+var wint = document.getElementById('1')
+wint = winter;
+
+function spring () {
+    background='green'
+}
+io.sockets.emit('send matrix', matrix)
+var sprg = document.getElementById('2')
+sprg = spring;
+
+function summer () {
+    background='red'
+}
+io.sockets.emit('send matrix', matrix)
+var summr = document.getElementById('3')
+summr = summer;
+
+function autumn () {
+    background='yellow'
+}
+io.sockets.emit('send matrix', matrix)
+var autm = document.getElementById('4')
+autm = autumn;
+
+
+
+function addGrass() {
+    for (var i = 0; i < 7; i++) {
+    var x = Math.floor(Math.random() * matrix[0].length)
+    var y = Math.floor(Math.random() * matrix.length)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 1
+            var gr = new Grass(x, y, 1)
+            grassArr.push(gr)
+        }
+    }
+}
+io.sockets.emit('send matrix', matrix)
+var addgr = document.getElementById('5')
+addgr = addGrass;
+
+function addGrassEater() {
+    for (var i = 0; i < 7; i++) {
+    var x = Math.floor(Math.random() * matrix[0].length)
+    var y = Math.floor(Math.random() * matrix.length)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 2
+            var gr = new GrassEater(x, y, 1)
+            grassEaterArr.push(gr)
+        }
+    }
+}
+io.sockets.emit('send matrix', matrix)
+var addgre = document.getElementById('6')
+addgre = addGrassEater; 
+
+function addPredator() {
+    for (var i = 0; i < 7; i++) {
+    var x = Math.floor(Math.random() * matrix[0].length)
+    var y = Math.floor(Math.random() * matrix.length)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 3
+            var gr = new Predator(x, y, 1)
+            predatorArr.push(gr)
+        }
+    }
+}
+io.sockets.emit('send matrix', matrix)
+var addpre = document.getElementById('7')
+addpre = addPredator; 
+
+function addCactus() {
+    for (var i = 0; i < 7; i++) {
+    var x = Math.floor(Math.random() * matrix[0].length)
+    var y = Math.floor(Math.random() * matrix.length)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 4
+            var gr = new Cactus(x, y, 1)
+            cactusArr.push(gr)
+        }
+    }
+}
+io.sockets.emit('send matrix', matrix)
+var addc = document.getElementById('8')
+addc = addCactus;
+
+function addHuman() {
+    for (var i = 0; i < 7; i++) {
+    var x = Math.floor(Math.random() * matrix[0].length)
+    var y = Math.floor(Math.random() * matrix.length)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 5
+            var gr = new Human(x, y, 1)
+            humanArr.push(gr)
+        }
+    }
+}
+io.sockets.emit('send matrix', matrix)
+var addhmn = document.getElementById('9')
+addhmn = addHuman;
+
+function bomb () {
+    grassArr = []
+    grassEaterArr = []
+    predatorArr = []
+    cactusArr = []
+    humanArr = []
+}
+io.sockets.emit('send matrix', matrix)
+var bomba = document.getElementById('10')
+bomba = bomb;
+
+function supchar () {
+     var spc = Math.floor(rand(2,5))
+    if(spc == 2) {
+    var randg = rand(grassEaterArr.length)
+    randg(this.energy+=100)
+    }
+    if(spc == 3) {
+        var randp = rand(predatorArr.length)
+    randp(this.energy+=100)
+    }
+    if(spc == 4) {
+        var randc = rand(cactusArr.length)
+    randc(this.energy+=100)
+    }
+    if(spc == 5) {
+        var randh = rand(humanArr.length)
+    randh(this.energy+=100)
+    }
+}
+io.sockets.emit('send matrix', matrix)
+var spch = document.getElementById('11')
+spch = supchar; 
